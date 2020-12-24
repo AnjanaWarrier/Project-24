7e5d588d2f3f1981ff61de43334cdad1e26a98e4
AnjanaWarrier <74911685+AnjanaWarrier@users.noreply.github.com>
2020-12-24 18:23:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paper1 = new Paper(200,200,30);
+	ground = new Ground(400,650,800,5);
+
+	box1 = new Box(600,650,200,20);
+	box2 = new Box(700,600,20,75);
+	box3 = new Box(500,600,20,75);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  
+  paper1.display();
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85,y:-85});
+	}
+}
+
